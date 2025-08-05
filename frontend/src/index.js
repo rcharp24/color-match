e64b13aabdac81cd977fb83609f0cc1c3c1140bc
rcharp24 <robj.charpentier@gmail.com>
2025-08-04 20:06:52 -0500

    Add .gitignore and remove node_modules from tracking and push everything to main

---
+++ frontend/src/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+
+root.render(
+  <React.StrictMode>
+    <div style={{ textAlign: 'center', marginTop: '50px' }}>
+      <h1>🎨 Color Match App</h1>
+      <p>Frontend is working!</p>
+    </div>
+  </React.StrictMode>
+);
